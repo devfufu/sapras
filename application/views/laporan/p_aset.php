83cebfe1f4f53880c890101883b117c351c4029c
unknown <fuadfadilah99@gmail.com>
2026-07-13 12:40:24 +0700

    ok

--- application/views/laporan/p_aset.php
+++ application/views/laporan/p_aset.php
@@ -17,39 +17,121 @@
     font-weight: bold;
 }
 
-@media print {
-    table thead th {
-        text-align: center !important;
-        vertical-align: middle !important;
-    }
+.table {
+    width: 100% !important;
+    table-layout: fixed;
+    border-collapse: collapse !important;
+}
 
-    img {
-        max-width: 80px;
-    }
+.table th,
+.table td {
+    border: 1px solid #000 !important;
+    padding: 5px;
+    vertical-align: middle;
+    word-wrap: break-word;
 }
 
 .table thead th {
     text-align: center !important;
     vertical-align: middle !important;
 }
+
+img {
+    max-width: 80px;
+    height: auto;
+}
+
+.ttd-table,
+.ttd-table tr,
+.ttd-table td {
+    border: none !important;
+}
+
+.col-no {
+    width: 3%;
+}
+
+.col-foto {
+    width: 10%;
+}
+
+.col-kode {
+    width: 10%;
+}
+
+.col-nama {
+    width: 16%;
+}
+
+.col-lokasi {
+    width: 10%;
+}
+
+.col-sumber {
+    width: 15%;
+}
+
+.col-volume {
+    width: 6%;
+}
+
+.col-satuan {
+    width: 6%;
+}
+
+.col-harga {
+    width: 11%;
+}
+
+.col-total {
+    width: 12%;
+}
+
+@media print {
+
+    @page {
+        size: A4 portrait;
+        margin: 10mm;
+    }
+
+    .container {
+        max-width: 100% !important;
+    }
+
+    .table {
+        width: 100% ! important;
+        border-collapse: colla pse !important;
+    }
+
+    .table th,
+    .table td {
+        border: 1px solid #000 !important;
+        padding: 3px !important;
+        font-size: 10px;
+        word-wrap: break-word;
+    }
+
+    .table img {
+        max-width: 50px;
+        height: auto;
+    }
+}
 </style>
 
 <body>
     <div class="container">
         <div class="row pt-4">
-            <div class="col-md-2">
-                <img src="<?= base_url() ?>src/img/logo/logo.png" class="kiri" alt="logo">
-            </div>
+
 
             <div class="col-md-10 text-center judul">
                 <h2 style="font-size: 18;">YAYASAN FADILAH</h2>
                 <h3>SMK FADILAH</h3>
-                <p>Jl. Pendidikan II, Parigi, Kec. Pd. Aren, Kota
-                    Tangerang Selatan, Banten 15227
-                </p>
+                <p>Jl. Pendidikan II, Parigi, Kec.
+                    Pd. Aren, Kota Tangerang Selatan, Banten 15227 </p>
             </div>
         </div>
-        <hr style="border:1px solid black;">
+        <hr style="border:1px solid
+                    black;">
         <div class="row">
             <div class="col text-center">
                 <strong class="judul">
@@ -63,54 +145,74 @@
                 </strong>
             </div>
         </div>
-        <div class="row pt-3">
+        <div class=" row pt-3">
             <div class="col">
                 <table class="table table-bordered">
                     <thead>
                         <tr class="text-center">
-                            <th>NO</th>
-                            <th>FOTO</th>
-                            <th>KODE ASET</th>
-                            <th>NAMA</th>
-                            <th>LOKASI</th>
-                            <th>SUMBER PEMBELIAN</th>
-                            <th>VOLUME</th>
-                            <th>SATUAN</th>
-                            <th>HARGA SATUAN <br> (Rp.)</th>
-                            <th>JUMLAH <br> (Rp.)</th>
+                            <th class="col-no">NO</th>
+                            <th class="col-foto">FOTO</th>
+                            <th class="col-kode">KODE ASET</th>
+                            <th class="col-nama">NAMA</th>
+                            <th class="col-lokasi">LOKASI</th>
+                            <th class="col-sumber">SUMBER PEMBELIAN</th>
+                            <th class="col-volume">VOLUME</th>
+                            <th class="col-satuan">SATUAN</th>
+                            <th class="col-harga">HARGA SATUAN <br>(Rp.)</th>
+                            <th class="col-total">JUMLAH <br>(Rp.)</th>
                         </tr>
                     </thead>
                     <tbody>
                         <?php
                         $sum = 0;
-                        $no  = 1;
+                        $no = 1;
 
                         foreach ($aset as $row):
 
                             $sum += $row['total_harga'];
-                        ?>
+                            ?>
 
                         <tr>
-                            <td><?= $no++; ?></td>
+                            <td class="text-center">
+                                <?= $no++; ?>
+                            </td>
                             <td class="text-center">
                                 <img src="<?= base_url('src/img/aset/' . $row['foto_aset']) ?>" alt="Foto Aset"
                                     width="80">
                             </td>
-                            <td><?= $row['kode_aset'] ?></td>
-                            <td><?= $row['nama_barang'] ?></td>
-                            <td><?= $row['nama_lokasi'] ?></td>
-                            <td><?= $row['jenis_bantuan'] ?></td>
-                            <td><?= $row['volume'] ?></td>
-                            <td><?= $row['satuan'] ?></td>
-                            <td><?= laporan($row['harga']) ?></td>
-                            <td><?= laporan($row['total_harga']) ?></td>
+                            <td>
+                                <?= $row['kode_aset'] ?>
+                            </td>
+                            <td>
+                                <?= $row['nama_barang'] ?>
+                            </td>
+                            <td>
+                                <?= $row['nama_lokasi'] ?>
+                            </td>
+                            <td>
+                                <?= $row['jenis_bantuan'] ?>
+                            </td>
+                            <td class="text-center">
+                                <?= $row['volume'] ?>
+                            </td>
+                            <td class="text-center">
+                                <?= $row['satuan'] ?>
+                            </td>
+                            <td>
+                                <?= laporan($row['harga']) ?>
+                            </td>
+                            <td>
+                                <?= laporan($row['total_harga']) ?>
+                            </td>
                         </tr>
 
                         <?php endforeach; ?>
 
                         <tr>
                             <td colspan="9"><b>Total</b></td>
-                            <td><b><?= laporan($sum) ?></b></td>
+                            <td><b>
+                                    <?= laporan($sum) ?>
+                                </b></td>
                         </tr>
 
                     </tbody>
@@ -121,7 +223,7 @@
         <div class="row pt-5">
             <div class="col-12">
 
-                <table width="100%">
+                <table width="100%" class="ttd-table">
                     <tr>
                         <td width="50%" align="left" style="font-family: 'Times New Roman', Times, serif;">
                             Mengetahui,<br>
@@ -133,7 +235,8 @@
                         </td>
 
                         <td width="50%" align="right" style="font-family: 'Times New Roman', Times, serif;">
-                            Tangerang Selatan, <?= tgl_indo(date('Y-m-d')) ?><br>
+                            Tangerang Selatan,
+                            <?= tgl_indo(date('Y-m-d')) ?><br>
                             WK. Sapras
                             <br><br><br><br><br>
 
@@ -155,7 +258,7 @@
 
     window.onafterprint = function() {
 
-        window.location.href = "<?= base_url('laporan/search_asetRange') ?>";
+        window.location.href = "<?= base_url('laporan/aset') ?>";
 
     };
     </script>
